267bb958c6dcb784d52b177eb244381ef1291e12
nurul khasanah <nurulkkhasanah647@gmail.com>
2022-08-08 14:42:35 +0700

    aplikasi jurnal prakerin

---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title>Aplikasi jurnal prakerin</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    </head>
+    <body class="bg-info">
+
+<div class="row">  
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+             <!-- form login -->
+        <div class="card p-3" style="margin-top: 80px">
+             <div class="h-login text-center">
+                <h5>Form Login</h5>
+                <h3>Aplikasi Jurnal Prakerin</h3>
+                <hr class="col-md-5 mx-auto mb-5">
+             </div>
+             <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary col-md-12">Login</button>
+                </form>
+                    </div>
+                <div class="f-login text-center mt-5">
+                    Login With <br>
+                    <small>Google </small> | <small>Facebook</small>
+                </div>
+            </div>
+            
+            </div>
+            <div class="col-md-5"></div>
+    </div>
+
+    </body>
+</html>
